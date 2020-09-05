a133e36ac96cb38610d8332355ad687c9f37422f
CS HUI <cshui484@gmail.com>
2020-09-05 16:05:45 -0500

    added PostgreSQL env var and psycopg2

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -10,7 +12,7 @@ from db import db
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URl', 'sqlite:///data.db')
 # turn off the tracker in Flask_SQLAlchemy, uses the one in SQLAlchemy
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
